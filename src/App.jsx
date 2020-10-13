8eaf909042d255a3aa6ddae9eff525eaf88d4125
sumaqkuyay <sabina.canchumani.huaman@gmail.com>
2020-10-13 17:20:19 -0500

    instalación de dependencias y creación de boilerplate del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  BrowserRouter as Router, Route, Switch, HashRouter,
+} from 'react-router-dom';
+import Login from './view/Login';
+import './sass/index.scss';
+
+const App = () => (
+  <Router>
+    <HashRouter>
+      <Switch>
+        <Route path="/" exact component={Login} />
+      </Switch>
+    </HashRouter>
+  </Router>
+);
+
+export default App;
---
+++ src/view/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Login = () => {
+
+  return (
+    <div className="col-sm-8 col-md-6 col-lg-4 col-xl-3">
+      <div className="d-flex justify-content-center">
+        <img className="size-img mt-2" alt="Logo Yanbal" />
+      </div>
+      <form className="text-center" >
+        <input type="email" name="email" className="form-control my-3 bottom px-0" placeholder="Correo" required />
+        <input type="password" name="pass" className="form-control my-3 bottom px-0" placeholder="Contraseña" required />
+        <button className="btn btn-primary" type="submit">Ingresar</button>
+      </form>
+    </div>
+  );
+};
+export default Login;
