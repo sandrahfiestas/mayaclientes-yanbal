c282f4ab98493ca76035a33021f2b4535e4c9b6d
jael-VRR <jrodriguezruidias@gmail.com>
2020-10-13 18:30:36 -0500

    sdk firefase

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDEJT0L9tzltTboF5A2r8asfOCG-THWFnI",
-  authDomain: "react-burger-queen.firebaseapp.com",
-  databaseURL: "https://react-burger-queen.firebaseio.com",
-  projectId: "react-burger-queen",
-  storageBucket: "react-burger-queen.appspot.com",
-  messagingSenderId: "854719954286",
-  appId: "1:854719954286:web:c84bb1ec4cd7932688557f"
+    apiKey: "AIzaSyCm-w-DMQBq10Xn2QzzM-KZ7GdLtOfx218",
+    authDomain: "maya-clientes-55c0f.firebaseapp.com",
+    databaseURL: "https://maya-clientes-55c0f.firebaseio.com",
+    projectId: "maya-clientes-55c0f",
+    storageBucket: "maya-clientes-55c0f.appspot.com",
+    messagingSenderId: "753767093893",
+    appId: "1:753767093893:web:55b7c863ce6920a1a97bec"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
   export {firebase}
+ 
--- src/controller/firebase.js
+++ src/controller/firebase.js
@@ -1,10 +1,10 @@
 import {firebase} from '../firebase'
 
 
-// agrega un objeto a la colección orders con las siguientes propiedades client,products,total,fecha ,status y timer
+
 export const addOrder = (order) => firebase.firestore().collection('Orders').add(order)
 
-// result de la petición a firebase de la data almacenada
+
 export  const getOrders =(callback) =>  firebase.firestore().collection('Orders')
 
 .onSnapshot((snap) => {
@@ -20,7 +20,7 @@ export  const getOrders =(callback) =>  firebase.firestore().collection('Orders'
  
 });
 
-// modifica el valor asignado inicialmente en "pendiente" a listo al ejecutarse el evento checkListOrder
+
 export const updateStatusInOrder = (id,data)  => firebase.firestore().collection('Orders').doc(id).update(data)
 
 export const getProducts =(callback) =>firebase.firestore().collection('Products')
