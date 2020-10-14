920381c713074aae6e678a00ccafd6d876c56a42
jael-VRR <jrodriguezruidias@gmail.com>
2020-10-14 04:39:16 -0500

    login

--- src/view/Login.jsx
+++ src/view/Login.jsx
@@ -1,20 +1,36 @@
 import React from "react";
 import Logo from "../images/logo.png";
+import Vector from "../images/Vector.png";
 
 
 const Login = () => {
 
   return (
     <div className="container main">
-      <div className="d-flex justify-content-center">
-        <img className="size-img mt-2" src={Logo} alt="Logo Yanbal" />
+      <div className="row">
+        <div className="col-lg-11 col-md-1 col-12">
+          <div className="text-center">
+            <img className="img-ts img-fluid" src={Logo} alt="yanbal" />
+          </div>
+          <br/>
+          <form className="text-center">
+            <input type="email" name="email" className="form-control my-3 bottom px-0 " placeholder="Correo" required />
+            <input type="password" name="pass" className="form-control my-3 bottom px-0 " placeholder="Contraseña" required />
+            <br/>
+            <div>
+              <p className="text-center">Guardar Contraseña</p>
+            </div>
+            <button className="btn btn-primary login" type="submit">Ingresar</button>
+            <br/>
+          </form>
+          <br/>
+          <div>
+            <p className="text-center"><img src={Vector} alt=""/>Olvide mi contraseña</p>
+          </div>
+        </div>
       </div>
-      <form className="text-center">
-        <input type="email" name="email" className="form-control my-3 bottom px-0" placeholder="Correo" required />
-        <input type="password" name="pass" className="form-control my-3 bottom px-0" placeholder="Contraseña" required />
-        <button className="btn btn-primary" type="submit">Ingresar</button>
-      </form>
     </div>
+
   );
 };
 export default Login;
