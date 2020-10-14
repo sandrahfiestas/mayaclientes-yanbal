651b032d4cd9d3408561e20e327e079816df3f96
sumaqkuyay <sabina.canchumani.huaman@gmail.com>
2020-10-14 06:18:36 -0500

    eliminar archivos innecesarios y editar router para vista creación de cliente

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {
   BrowserRouter as Router, Route, Switch, HashRouter,
 } from 'react-router-dom';
 import Login from './view/Login';
+import CreateClient from './view/CreateClient';
 import './sass/index.scss';
 
 const App = () => (
@@ -10,6 +11,7 @@ const App = () => (
     <HashRouter>
       <Switch>
         <Route path="/" exact component={Login} />
+        <Route path="/client" exact component={CreateClient} />
       </Switch>
     </HashRouter>
   </Router>
