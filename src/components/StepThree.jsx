77d267bd04ba59abf7dd130259519030fd33791e
sumaqkuyay <sabina.canchumani.huaman@gmail.com>
2020-10-14 06:14:17 -0500

    Agregando Componente de registro de comentarios

---
+++ src/components/StepThree.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+
+export class StepThree extends Component {
+    continue = e => {
+        e.preventDefault();
+        this.props.nextStep();
+    };
+
+    back = e => {
+        e.preventDefault();
+        this.props.prevStep();
+    };
+
+    render() {
+        const { values, inputChange } = this.props;
+
+        return (
+            <div className="form-container">
+                <h1 className="mb-5">Agregar comentarios o información de cliente</h1>
+                <div className="form-group">
+                <textarea className="box" placeholder="Ingresa descripción" name="description" />
+                    <label htmlFor="email">Email</label>
+                    <input type="email" className="form-control" name="email" onChange={inputChange('email')} value={values.email} />
+                </div>
+        
+                <br />
+
+                <div className="row">
+                    <div className="col-6">
+                        <button className="btn btn-danger" onClick={this.back}>Back</button>
+                    </div>
+                    <div className="col-6 text-right">
+                        <button className="btn btn-primary" onClick={this.continue}>Continue</button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default StepThree;
