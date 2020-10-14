24daccfb60d44e19a70e811decb36fd8056b6840
sumaqkuyay <sabina.canchumani.huaman@gmail.com>
2020-10-13 19:26:54 -0500

    change

--- src/view/Login.jsx
+++ src/view/Login.jsx
@@ -7,7 +7,7 @@ const Login = () => {
       <div className="d-flex justify-content-center">
         <img className="size-img mt-2" alt="Logo Yanbal" />
       </div>
-      <form className="text-center" >
+      <form className="text-center">
         <input type="email" name="email" className="form-control my-3 bottom px-0" placeholder="Correo" required />
         <input type="password" name="pass" className="form-control my-3 bottom px-0" placeholder="Contraseña" required />
         <button className="btn btn-primary" type="submit">Ingresar</button>
