eb5151b8445098a2d82f196a3244d8af601c5ed2
sandrahfiestas <shfiestas@gmail.com>
2020-10-15 07:43:18 -0500

    eliminando comentarios adicionales

--- src/components/StepOne.jsx
+++ src/components/StepOne.jsx
@@ -16,13 +16,12 @@ export class StepOne extends Component {
                   <h1 className="mb-5">¿Cómo se llama tu cliente?</h1>
                 </div>
                 <div className="form-group form-register">
-                    {/* <label htmlFor="name">Name</label> */}
                     <input type="text" className="form-control input-register" placeholder="nombres" name="name" onChange={inputChange('name')} value={values.name} />
                     <input type="text" className="form-control input-register" placeholder="apellidos" name="lastName" />
                 </div>
                 <br />
-                <div className="text-right">
-                    <button className="btn btn-primary btn-next" onClick={this.continue}>
+                <div className="div-btn">
+                    <button className="btn btn-primary btn-form" onClick={this.continue}>
                     <p className="txt-btn-form">Siguiete</p>
                     <img src={btnNext} className="icon-next-form" alt="icon-next-form" />
                     </button>
