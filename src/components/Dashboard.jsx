c76ac0f619f69c0d80d766336e9fbde297ce06d9
sandrahfiestas <shfiestas@gmail.com>
2020-10-16 01:07:50 -0500

    dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,52 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 
 function Dashboard() {
+    const [data, setData] = useState([0]);
+
+    //lectura del json
+    useEffect(() => {
+        fetch('../products.json')
+          .then((response) => response.json())
+          .then((datos) => {
+            setData(datos);
+            console.log(datos);
+          });
+      });
+
     return (
         <>
-            <div className="nav">
-             <button className="btn-nav" >
-                    <a>Clientes</a>
+         <div className="nav">
+              <button className="btn-nav" >
+                <a>Clientes</a>
+               </button>
+                <button className="btn-nav">
+                  <a>Cobranzas</a>
                 </button>
                 <button className="btn-nav">
-                    <a>Cobranzas</a>
+                  <a>Oportunidades</a>
                 </button>
-                <button className="btn-nav">
-                    <a>Oportunidades</a>
-                </button>
-                <div>
+            <div>
 
                 </div>
             </div>
-      
             <div className="div-cards">
                 <p className="txt-card">Estos productos podría interesarle a tus clientes.</p>
+           
                 <div className="card">
-                    <p className="description-card">A María Santos, podría interesarle estos productos</p>
-                    <div>
-                        <img src="" alt=""/>
-                        <img src="" alt=""/>
-                        <img src="" alt=""/>
-                        <div>
-                            <img src="" alt=""/>
-                            <img src="" alt=""/>
-                        </div>
+                 <p className="description-card">A María Santos, podría interesarle estos productos</p>
+                    <div className="div-items">
+                       <img className="img" src='https://user-images.githubusercontent.com/60890508/96071170-2b8ccc00-0e67-11eb-8613-071b328a064b.jpg' alt="imagen" /> 
+                       <img className="img" src='https://user-images.githubusercontent.com/60890508/96071166-2a5b9f00-0e67-11eb-9e17-4432c6b5154d.jpg' alt="imagen" /> 
+                       <img className="img" src='https://user-images.githubusercontent.com/60890508/96071048-fd0ef100-0e66-11eb-9da2-b148efcb2625.jpg' alt="imagen" />  
                     </div>
                 </div>
 
                 <div className="card">
-                    <p className="description-card">A María Santos, podría interesarle estos productos</p>
-                    <div>
-                        <img src="" alt=""/>
-                        <img src="" alt=""/>
-                        <img src="" alt=""/>
-                        <div>
-                            <img src="" alt=""/>
-                            <img src="" alt=""/>
-                        </div>
+                 <p className="description-card">A María Santos, podría interesarle estos productos</p>
+                    <div className="div-items">
+                       <img className="img" src='https://user-images.githubusercontent.com/60890508/96071139-1f087380-0e67-11eb-92ed-5a20b5ab383e.jpg' alt="imagen" /> 
+                       <img className="img" src='https://user-images.githubusercontent.com/60890508/96071226-46f7d700-0e67-11eb-8106-b8083fcf62cb.jpg' alt="imagen" /> 
+                       <img className="img" src='https://user-images.githubusercontent.com/60890508/96071052-fed8b480-0e66-11eb-8464-06ed4c9a3e0a.jpg' alt="imagen" />  
                     </div>
                 </div>
+
             </div>
         </>
     );
